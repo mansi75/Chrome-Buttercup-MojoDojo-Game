530d47843feedb48ea5b580b43129c89cd6efb11
mansi75 <51503378+mansi75@users.noreply.github.com>
2021-01-10 17:34:56 +0530

    Update mojodojo.js

--- mojodojo.js
+++ mojodojo.js
@@ -1,1 +1,18 @@
-
+class Mojodojo {
+  
+  constructor() {
+    this.r = 50;
+    this.x = width;
+    this.y = height - this.r;
+  
+  }
+  
+  move() {
+    this.x -= 8;
+  }
+  
+  show() {
+    image(mImg, this.x, this.y, this.r, this.r);
+    
+  }
+}
